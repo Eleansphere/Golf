d5412ea726c3b74bf1bec16e637a8f21d753c497
kuba-stastny <66886387+kuba-stastny@users.noreply.github.com>
2022-06-26 23:25:29 +0200

    Responzivita

--- parts/services.php
+++ parts/services.php
@@ -37,16 +37,19 @@
                 </div>
             </div>
             <div class="left-flex-center article-title-bg">
-                KARIÉRNÍ PLÁNOVÁNÍ
+                PLÁNOVÁNÍ A ORGANIZAČNÍ ZAJIŠTĚNÍ
             </div>
         </div>
         <div class="article-text">
-            Pomůžeme Vám najít optimální
-            cestu k dosažení Vašeho
-            profesionálního sportovního cíle.
-            Pomůžeme Vám stanovovat
-            průběžné cíle, sledovat je,
-            podrobně vyhodnocovat,
+            Vytváříme prostředí a zázemí, ve kterém může
+            každý sportovec uskutečňovat své sny a
+            dosahovat svých cílů. Nabízíme nadstandartní
+            služby, špičkový servis, individuální přístup,
+            přátelské jednání a diskrétnost. Proto ke
+            každému sportovci přistupujeme individuálně a
+            s ojedinělým řešením, které vede k zajištění
+            maximálního komfortu, vzájemné důvěře a
+            spokojenosti sportovce.
 
         </div>
 
@@ -79,19 +82,16 @@
                 </div>
             </div>
             <div class="left-flex-center article-title-bg">
-                PLÁNOVÁNÍ A ORGANIZAČNÍ ZAJIŠTĚNÍ
+                KARIÉRNÍ PLÁNOVÁNÍ
             </div>
         </div>
         <div class="article-text">
-            Vytváříme prostředí a zázemí, ve kterém může
-            každý sportovec uskutečňovat své sny a
-            dosahovat svých cílů. Nabízíme nadstandartní
-            služby, špičkový servis, individuální přístup,
-            přátelské jednání a diskrétnost. Proto ke
-            každému sportovci přistupujeme individuálně a
-            s ojedinělým řešením, které vede k zajištění
-            maximálního komfortu, vzájemné důvěře a
-            spokojenosti sportovce.
+            Pomůžeme Vám najít optimální
+            cestu k dosažení Vašeho
+            profesionálního sportovního cíle.
+            Pomůžeme Vám stanovovat
+            průběžné cíle, sledovat je,
+            podrobně vyhodnocovat,
 
         </div>
 
