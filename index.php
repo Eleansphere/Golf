99d8c145b613b8a72fb0cc370f9929b1125b1221
kuba-stastny <66886387+kuba-stastny@users.noreply.github.com>
2022-07-11 20:37:06 +0200

    Empty

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Filip Sport Management</title>
-    <link rel="stylesheet" href="styles6.css">
+    <link rel="stylesheet" href="styles7.css">
 </head>
 
 
